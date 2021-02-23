14d89bb5ea7d6323d388053b88bbafc3f5522ec8
arvyjaar <arvydo.j@gmail.com>
2021-02-23 09:31:01 +0200

    Add more tests vor input validation

--- tests/Controller/GameControllerTest.php
+++ tests/Controller/GameControllerTest.php
@@ -18,18 +18,6 @@ class GameControllerTest extends WebTestCase
         $this->assertResponseStatusCodeSame(200);
     }
 
-    public function testIndexResponseContent(): void
-    {
-        $content = [
-            ['x', 'x', 'x'],
-            ['', '', 'o'],
-            ['', 'x', ''],
-        ];
-        $client = static::createClient();
-        $client->xmlHttpRequest('POST', '/game', [], [], [], json_encode(['board' => $content]));
-        $this->assertResponseStatusCodeSame(200);
-    }
-
     public function testIndexResponseContentWhenFullTable(): void
     {
         $content = [
@@ -40,5 +28,105 @@ class GameControllerTest extends WebTestCase
         $client = static::createClient();
         $client->xmlHttpRequest('POST', '/game', [], [], [], json_encode(['board' => $content]));
         $this->assertResponseStatusCodeSame(200);
+    }
+
+    public function testIndexResponseContentWhenEmptyTable(): void
+    {
+        $content = [
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', ''],
+        ];
+        $client = static::createClient();
+        $client->xmlHttpRequest('POST', '/game', [], [], [], json_encode(['board' => $content]));
+        $this->assertResponseStatusCodeSame(200);
+    }
+
+    public function testValidationNothingGiven(): void
+    {
+        $content = null;
+        $client = static::createClient();
+        $client->xmlHttpRequest('POST', '/game', [], [], [], json_encode($content));
+        $this->assertResponseStatusCodeSame(400);
+    }
+
+    public function testValidationStringGiven(): void
+    {
+        $content = ['board' => 'Some string'];
+        $client = static::createClient();
+        $client->xmlHttpRequest('POST', '/game', [], [], [], json_encode($content));
+        $this->assertResponseStatusCodeSame(400);
+    }
+    
+    public function testValidationLessThan3Rows(): void
+    {
+        $client = static::createClient();
+        $content = [
+            ['', 'x', ''],
+            ['', '', 'o'],
+        ];
+        $client->xmlHttpRequest('POST', '/game', [], [], [], json_encode(['board' => $content]));
+        $this->assertResponseStatusCodeSame(400);
+    }
+
+    public function testValidationMoreThan3Rows(): void
+    {
+        $client = static::createClient();
+        $content = [
+            ['', 'x', ''],
+            ['', '', 'x'],
+            ['', '', 'o'],
+            ['x', 'o', ''],
+        ];
+        $client->xmlHttpRequest('POST', '/game', [], [], [], json_encode(['board' => $content]));
+        $this->assertResponseStatusCodeSame(400);
+    }
+
+    public function testValidationLessThan3ItemsInRowSameLength(): void
+    {
+        $client = static::createClient();
+        $content = [
+            ['', 'x'],
+            ['', '',],
+            ['', '',],
+        ];
+        $client->xmlHttpRequest('POST', '/game', [], [], [], json_encode(['board' => $content]));
+        $this->assertResponseStatusCodeSame(400);
+    }
+
+    public function testValidationLessThan3ItemsInRowDifferentLength(): void
+    {
+        $client = static::createClient();
+        $content = [
+            ['', 'x'],
+            ['', '', 'o'],
+            ['', '',],
+        ];
+        $client->xmlHttpRequest('POST', '/game', [], [], [], json_encode(['board' => $content]));
+        $this->assertResponseStatusCodeSame(400);
+    }
+
+    public function testValidationMoreThan3ItemsInRow(): void
+    {
+        $client = static::createClient();
+        $content = [
+            ['', 'x', ''],
+            ['', '', 'x', 'o'],
+            ['', '', 'o'],
+        ];
+        $client->xmlHttpRequest('POST', '/game', [], [], [], json_encode(['board' => $content]));
+        $this->assertResponseStatusCodeSame(400);
+    }
+
+    public function testValidationInvalidUnit(): void
+    {
+        $client = static::createClient();
+        $content = [
+            ['', 'x', ''],
+            ['', 'Y', ''],
+            ['', '', 'o'],
+        ];
+        $client->xmlHttpRequest('POST', '/game', [], [], [], json_encode(['board' => $content]));
+        $this->assertResponseStatusCodeSame(400);
     }
 }
